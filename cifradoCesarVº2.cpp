eefd185e46309707d3f8d54db880d27570ec7010
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2021-04-23 00:01:56 -0500

    Create cifradoCesarVº2.cpp

---
+++ cifradoCesarVº2.cpp
@@ -0,0 +1,172 @@
+//  Actividad:  Cifrado Cesar    - Cifrado/Descifrado/Criptoanalisis
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+char arrayMay[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+char arrayMin[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+string libreriaPalabrasEspañol[250]={"Hola","Versace"};
+
+void Cifrado(string &mensajeAencriptar);
+void Descifrado(string &mensajeEncriptado);
+
+void criptoanalisis(string &mensaje);
+
+int main()
+{
+    string mensaje;
+    cout<<"Ingrese el mensaje:"<<endl;
+    getline(cin,mensaje);
+
+    Cifrado(mensaje);
+
+    cout<<"\n\n\n";
+
+    cout<<"Mensaje encriptado: "<<endl<<endl;
+    
+    cout<<mensaje<<endl<<endl;
+
+    Descifrado(mensaje);
+
+    cout<<"\n\n\n";
+
+    cout<<"EL mensaje desencriptado es : "<<endl<<endl;
+    
+    cout<<mensaje<<endl<<endl;
+    
+    return 0;
+}
+
+
+void Cifrado(string &mensajeAencriptar)
+{
+    int clave;
+    cout<<"Ingresar clave: ";
+    cin>>clave;
+    int encounter;
+    
+    for(unsigned int pasada1{0};pasada1<mensajeAencriptar.size();pasada1++)
+    {
+        if(isalpha(mensajeAencriptar[pasada1]))
+        {
+            if(isupper(mensajeAencriptar[pasada1]))
+            {
+                for(unsigned int a1{0};a1<26;a1++)
+                {
+                    if(mensajeAencriptar[pasada1]==arrayMay[a1])
+                    {
+                        encounter=a1;
+                        if((clave+encounter)<=25)
+                        {
+                            mensajeAencriptar[pasada1]=arrayMay[clave+encounter];
+                            break;
+                        }
+                        else
+                        {
+                            mensajeAencriptar[pasada1]=arrayMay[((clave+encounter)-26)];
+                            break;
+                        }
+                    }
+                }
+            }
+            if(islower(mensajeAencriptar[pasada1]))
+            {
+                for(unsigned int a2{0};a2<26;a2++)
+                {
+                    if(mensajeAencriptar[pasada1]==arrayMin[a2])
+                    {
+                        encounter=a2;
+                        if((clave+encounter)<=25)
+                        {
+                            mensajeAencriptar[pasada1]=arrayMin[clave+encounter];
+                            break;
+                        }
+                        else
+                        {
+                            mensajeAencriptar[pasada1]=arrayMin[((clave+encounter)-26)];
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+void Descifrado(string &mensajeEncriptado)
+{
+    int clave2;
+    cout<<"Ingresar clave para desencriptar: ";
+    cin>>clave2;
+    int encounter;
+    
+    for(unsigned int pasada1{0};pasada1<mensajeEncriptado.size();pasada1++)
+    {
+        if(isalpha(mensajeEncriptado[pasada1]))
+        {
+            if(isupper(mensajeEncriptado[pasada1]))
+            {
+                for(unsigned int a1{0};a1<26;a1++)
+                {
+                    if(mensajeEncriptado[pasada1]==arrayMay[a1])
+                    {
+                        encounter=a1;
+                        if((encounter-clave2)>=0)
+                        {
+                            mensajeEncriptado[pasada1]=arrayMay[encounter-clave2];
+                            break;
+                        }
+                        else
+                        {
+                            mensajeEncriptado[pasada1]=arrayMay[(26-(clave2-encounter))];
+                            break;
+                        }
+                    }
+                }
+            }
+            if(islower(mensajeEncriptado[pasada1]))
+            {
+                for(unsigned int a2{0};a2<26;a2++)
+                {
+                    if(mensajeEncriptado[pasada1]==arrayMin[a2])
+                    {
+                        encounter=a2;
+                        if((encounter-clave2)>=0)
+                        {
+                            mensajeEncriptado[pasada1]=arrayMin[encounter-clave2];
+                            break;
+                        }
+                        else
+                        {
+                            mensajeEncriptado[pasada1]=arrayMin[(26-(clave2-encounter))];
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+void criptoanalisis(string &mensaje)
+{
+    string palabras123[100];
+    string mensajePivote=mensaje;
+    char espacio=' ';
+    int posiciones[40];
+    int b{0};
+    for(unsigned int a{0};a<(mensaje.size());a++)
+    {
+        if((mensaje[a])==espacio)
+        {
+            posiciones[b]=a;
+            b++;
+        }
+    }
+}
